03f8e5cb07be823d4fbe0da47cec8901260743b4
MartinFarres <martinfarres@gmail.com>
2024-08-27 14:31:52 -0300

    BD Proyect

---
+++ Code/BDTest/src/main/java/com/mycompany/bdtest/BDTest.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bdtest;
+
+
+/**
+ *
+ * @author tats
+ */
+public class BDTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
